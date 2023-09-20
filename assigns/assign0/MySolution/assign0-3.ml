920bf7729437d6196fa5127e30b5b83b7fc8516f
Krishin Wadhwani <krishin@bu.edu>
2023-09-20 12:18:04 -0400

    assign0-3 attempt 2

--- assigns/assign0/MySolution/assign0-3.ml
+++ assigns/assign0/MySolution/assign0-3.ml
@@ -1,15 +1,6 @@
-(* ****** ****** *)
-let chr = Char.chr
-let ord = Char.code
-let str(c0) = String.make 1 c0
-;;
-(* ****** ****** *)
-let string_init = String.init
-let string_length = String.length
-let string_get(cs, i0) = String.get cs i0
-;;
-(* ****** ****** *)
+#use "../../../classlib/OCaml/MyOCaml.ml";;
+
 let rec int2str (i0: int): string =
   if i0 < 10 then str(chr(i0+48))
-  else int2str(i0/10) ^ str(chr((i0 mod 10) + 48))
+  else string_snoc(int2str(i0/10))(chr((i0 mod 10) + 48))
 ;;
