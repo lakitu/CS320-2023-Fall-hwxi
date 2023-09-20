a43737ac214a4538fc6c7c5a3791f8ffcfef96fe
Krishin Wadhwani <krishin@bu.edu>
2023-09-20 00:00:47 -0400

    first try at fixing assign0

--- assigns/assign0/MySolution/assign0-2.ml
+++ assigns/assign0/MySolution/assign0-2.ml
@@ -6,9 +6,16 @@ let rec isPrimeLoop (n0: int) (i: int): bool =
 ;;
 
 let isPrime (n0: int): bool =
-  if n0 = 2 then true
-  else if n0 = 3 then true
+  if n0 < 2 then false
+  else if n0 <= 3 then true
   else if n0 mod 2 = 0 then false
   else if n0 mod 3 = 0 then false
   else isPrimeLoop(n0) (12)
 ;;
+
+isPrime(-1);;
+(* isPrime(2);;
+isPrime(5);;
+isPrime(6);;
+isPrime(91);;
+isPrime(89);; *)
--- assigns/assign0/MySolution/assign0-3.ml
+++ assigns/assign0/MySolution/assign0-3.ml
@@ -1,5 +1,14 @@
-#use "./../assign0.ml";;
-
+(* ****** ****** *)
+let chr = Char.chr
+let ord = Char.code
+let str(c0) = String.make 1 c0
+;;
+(* ****** ****** *)
+let string_init = String.init
+let string_length = String.length
+let string_get(cs, i0) = String.get cs i0
+;;
+(* ****** ****** *)
 let rec int2str (i0: int): string =
   if i0 < 10 then str(chr(i0+48))
   else int2str(i0/10) ^ str(chr((i0 mod 10) + 48))
