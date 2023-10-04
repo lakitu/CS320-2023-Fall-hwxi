afc81d62ea635c4985da99d1bf689dc41bfae9b6
Krishin Wadhwani <krishin@bu.edu>
2023-10-04 18:14:56 -0400

    fixed assign3-2.ml so now it works for real

--- assigns/assign3/MySolution/OCaml/assign3-2.ml
+++ assigns/assign3/MySolution/OCaml/assign3-2.ml
@@ -1,4 +1,5 @@
 #use "../../../../classlib/OCaml/MyOCaml.ml";;
+#use "../../../assign2/assign2.ml";;                                                             
 
 let in_list (xs: 'a list)(x: 'a): bool =
   not (list_forall(xs)(fun xi -> xi != x))
@@ -12,13 +13,28 @@ let pow b e =
   list_foldleft(int1_listize(e))(1)(fun r0 xi -> r0 * b)
 ;;
 
+let
+foldleft_to_iforeach
+( foldleft
+: ('xs,'x0,int) foldleft): ('xs,'x0) iforeach =
+fun xs iwork ->
+let _ = foldleft(xs)(0)(fun i x -> (iwork(i)(x); i+1)) in ()                           
+(* ****** ****** *)
+let
+list_iforeach =
+fun xs -> foldleft_to_iforeach(list_foldleft)(xs)
+
 let rec list_subsets (xs: 'a list): 'a list list =
   list_make_fwork(fun outerWork -> 
-    list_foreach(xs)(fun xi -> outerWork(
+    list_foreach
+      (int1_listize(pow(2)(list_len(xs))))
+      (fun i -> outerWork(
       list_make_fwork(fun innerWork -> 
-        for i0 = 1 to pow(2)(list_len(xs)-1) do 
-          innerWork(xi)
-        done
+        list_iforeach(xs)(fun j xj ->
+          if i land pow(2)(j) > 0 then
+          innerWork(xj)
+          else ()
+        )
       )
     ))
   )
